3e9edc5cc6e5b8f2469965be1ac1f4da40827d50
jesciak <jesciak@yahoo.com>
2022-11-22 21:01:32 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Bootcamp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jesciak_admin:admin!@cluster0.buz5hyz.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
